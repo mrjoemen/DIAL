e46f094c8e4dc01a12a338f18db875daf766daef
Jose Cabral <jose12marte@yahoo.com>
2020-08-21 10:50:35 -0400

    Updates

---
+++ precache-manifest.24c3e87a506d2a9a77891cc970cae579.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bfdc6131c212edbf18b496dcc68a13",
+    "url": "/DIAL/index.html"
+  },
+  {
+    "revision": "f39d9a8fb3d0a9e799b4",
+    "url": "/DIAL/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "28106b45b2b258103a1d",
+    "url": "/DIAL/static/css/main.eb6bf67c.chunk.css"
+  },
+  {
+    "revision": "f39d9a8fb3d0a9e799b4",
+    "url": "/DIAL/static/js/2.195b39e6.chunk.js"
+  },
+  {
+    "revision": "48708d0bba57d355d4ffc14a8bfac5cf",
+    "url": "/DIAL/static/js/2.195b39e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28106b45b2b258103a1d",
+    "url": "/DIAL/static/js/main.920f3c15.chunk.js"
+  },
+  {
+    "revision": "e2404b941cad6b1e9003",
+    "url": "/DIAL/static/js/runtime-main.639c947c.js"
+  },
+  {
+    "revision": "8cb3625b674ad5f967bfd404ab74bcda",
+    "url": "/DIAL/static/media/DAILWhiteLetters.8cb3625b.svg"
+  },
+  {
+    "revision": "20a56a7782c8ea5a991f56d1e42bde82",
+    "url": "/DIAL/static/media/HelenaLopes.20a56a77.jpg"
+  },
+  {
+    "revision": "05ae4838927d685347360b2c2102d300",
+    "url": "/DIAL/static/media/NaassomAzevedo.05ae4838.jpg"
+  },
+  {
+    "revision": "f27e7b9f0fc0b37cd83161990a59560c",
+    "url": "/DIAL/static/media/hexagon-green.f27e7b9f.svg"
+  },
+  {
+    "revision": "93ad96e270049decb3bd1fd7042480b8",
+    "url": "/DIAL/static/media/hexagon-white.93ad96e2.svg"
+  },
+  {
+    "revision": "e5ea3a6c999c0cf012391284205fc466",
+    "url": "/DIAL/static/media/implant.e5ea3a6c.jpg"
+  },
+  {
+    "revision": "339ad1e29abc5ee6ffcc1ebf474a9723",
+    "url": "/DIAL/static/media/myphoto2.339ad1e2.jpg"
+  },
+  {
+    "revision": "4ff053bce06ffd3d6982e188bdd109dc",
+    "url": "/DIAL/static/media/pops2.4ff053bc.jpg"
+  },
+  {
+    "revision": "48a230071cb9505ddffcde6f001fcb36",
+    "url": "/DIAL/static/media/removable.48a23007.jpg"
+  },
+  {
+    "revision": "46767d844ba9879b16fd3aed087605d6",
+    "url": "/DIAL/static/media/slide1.46767d84.jpg"
+  },
+  {
+    "revision": "2cdb29df11ecab2a34a23581d25b2fca",
+    "url": "/DIAL/static/media/slide2.2cdb29df.jpg"
+  },
+  {
+    "revision": "36ab141d76c17f44ed643e0d97905ae9",
+    "url": "/DIAL/static/media/slide3.36ab141d.jpg"
+  }
+]);
